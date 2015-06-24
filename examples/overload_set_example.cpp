f3b33eb49072698b4cbda93e858d6577a9e0b151
milleniumbug <wojas1222@gmail.com>
2015-06-24 18:58:09 +0200

    improved overload set - it's using decltype(auto) now

--- examples/overload_set_example.cpp
+++ examples/overload_set_example.cpp
@@ -6,14 +6,15 @@
 #include <wiertlo/range.hpp>
 
 template<typename T>
-void print(const T& x)
+std::ostream& print(const T& x)
 {
-	std::cout << x << "\n";
+	return std::cout << x << "\n";
 }
 
 int main()
 {
 	std::vector<int> a = { 2, 3, -42 };
 	//std::for_each(a.begin(), a.end(), print); // fails to compile
-	std::for_each(WIERTLO_RANGE(a), WIERTLO_OVERLOAD_SET(print));
+	std::for_each(WIERTLO_RANGE(a), WIERTLO_OVERLOAD_SET(print)); // standard algorithms taking functions
+	WIERTLO_OVERLOAD_SET(print)("Hello") << "Hi there!\n"; // testing reference returning
 }
--- include/wiertlo/overload_set.hpp
+++ include/wiertlo/overload_set.hpp
@@ -2,8 +2,9 @@
 #define HEADER003A3D7920834CF9BB4DE19FB45139AD
 
 #include <utility>
+#include <type_traits>
 
 // Emulation of Xeo's overload set proposal with macros and C++14 polymorphic lambdas
-#define WIERTLO_OVERLOAD_SET(function) ([](auto&&... x){ return (function)(std::forward<decltype(x)>(x)...); })
+#define WIERTLO_OVERLOAD_SET(function) ([](auto&&... x) -> decltype(auto) { return (function)(std::forward<decltype(x)>(x)...); })
 
 #endif
