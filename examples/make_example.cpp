487ed9371d51a7fceae2cc3f04cb69344d33a91e
milleniumbug <wojas1222@gmail.com>
2015-08-07 01:53:21 +0200

    library for common factory functions

---
+++ examples/make_example.cpp
@@ -0,0 +1,40 @@
+#include <algorithm>
+#include <iterator>
+#include <cassert>
+#include <wiertlo/make.hpp>
+#include <wiertlo/overload_set.hpp>
+
+int main()
+{
+	// wiertlo::make_array
+	// will fail because of no explicit type
+	// auto a = wiertlo::make_array(1, 2, 3);
+	auto arr = wiertlo::make_array<int>(42, 2, 1, 5);
+	static_assert(std::is_same<decltype(arr), std::array<int, 4>>::value, "");
+
+	// wiertlo::make:
+	// wiertlo::make<T>(...) is equivalent to T(...), but it's a function
+	// so it has no issues with MVP or passing to standard algorithms
+	std::vector<std::vector<int>> a_vector;
+	std::transform(arr.begin(), arr.end(), std::back_inserter(a_vector), WIERTLO_OVERLOAD_SET(wiertlo::make<std::vector<int>>));
+	for(std::size_t i = 0; i < arr.size(); ++i)
+	{
+		assert(arr[i] == static_cast<int>(a_vector[i].size()));
+	}
+	// triggers ICE in g++ 4.9.2
+	// std::transform(arr.begin(), arr.end(), std::back_inserter(a_vector), WIERTLO_OVERLOAD_SET(std::vector<int>));
+
+	// wiertlo::make_unique
+	// C++14's std::make_unique backport to C++11
+	// see http://en.cppreference.com/w/cpp/memory/unique_ptr/make_unique
+	auto po = wiertlo::make_unique<int>(42);
+	static_assert(std::is_same<decltype(po), std::unique_ptr<int>>::value, "");
+	auto pua = wiertlo::make_unique<int[]>(42);
+	static_assert(std::is_same<decltype(pua), std::unique_ptr<int[]>>::value, "");
+	// will fail to compile, just like C++14's std::make_unique
+	// auto pka = wiertlo::make_unique<int[5]>(42);
+
+	// wiertlo::make_shared is declared for symmetry with wiertlo::make_unique
+	// it just forwards the job to std::make_shared
+	auto sp = wiertlo::make_shared<int>(42);
+}
---
+++ include/wiertlo/make.hpp
@@ -0,0 +1,61 @@
+#include <utility>
+#include <memory>
+#include <array>
+#include <type_traits>
+
+namespace wiertlo
+{
+	namespace detail
+	{
+		template<typename T>
+		struct SuppressTemplateDeduction
+		{
+			typedef T type;
+		};
+
+		template<typename T>
+		struct DependentFalse
+		{
+			static const bool value = false;
+		};
+	}
+
+	template<typename T, typename... Args>
+	T make(Args&&... args)
+	{
+		return T(std::forward<Args>(args)...);
+	}
+
+	template<typename T, typename... Args>
+	std::array<T, sizeof...(Args)> make_array(Args&&... args)
+	{
+		std::array<T, sizeof...(Args)> arr = { std::forward<T>(args)... };
+		return arr;
+	}
+
+	template<typename T, typename... Args, typename std::enable_if<!std::is_array<T>::value>::type* = nullptr>
+	std::unique_ptr<T> make_unique(Args&&... args)
+	{
+		return std::unique_ptr<T>(new T(std::forward<Args>(args)...));
+	}
+
+	template<typename T, typename std::enable_if<std::is_array<T>::value && std::extent<T>::value == 0>::type* = nullptr>
+	std::unique_ptr<typename std::remove_extent<T>::type[]> make_unique(std::size_t size)
+	{
+		return std::unique_ptr<typename std::remove_extent<T>::type[]>(new typename std::remove_extent<T>::type[size]());
+	}
+
+	template<typename T, typename std::enable_if<std::is_array<T>::value && std::extent<T>::value != 0>::type* = nullptr>
+	void make_unique(std::size_t size)
+	{
+		static_assert(detail::DependentFalse<T>::value, "wiertlo::make_unique of array with a known bound is disallowed");
+	}
+
+	// make_shared is declared for symmetry with make_unique
+	template<typename T, typename... Args>
+	std::shared_ptr<T> make_shared(Args&&... args)
+	{
+		return std::make_shared<T>(std::forward<Args>(args)...);
+	}
+	
+}
