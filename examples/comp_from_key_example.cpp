9282db03e61f97e46131b104b371b080b3eacd6c
ennmichael <enntheprogrammer@gmail.com>
2017-02-12 16:03:51 +0100

    Added consts because I'm a douschebag

--- examples/comp_from_key_example.cpp
+++ examples/comp_from_key_example.cpp
@@ -16,7 +16,7 @@ int main()
 	{
 		std::vector<std::vector<int>> vecvec = { { 0 }, {}, { 0, 42, 23, 55 }, { 0, 0 } };
 		std::sort(vecvec.begin(), vecvec.end(), wiertlo::comp_from_key([](auto&& vec){ return vec.size(); }));
-		for(auto&& vec : vecvec)
+		for(const auto& vec : vecvec)
 		{
 			std::cout << vec.size() << "\n";
 		}
@@ -25,7 +25,7 @@ int main()
 	{
 		std::vector<Person> people = { Person{"John", "Rambo", 42}, Person{"Joffrey", "Baratheon", 14}, Person{"Kyle", "Reese", 25} };
 		std::sort(people.begin(), people.end(), wiertlo::comp_from_key(std::mem_fn(&Person::age), std::greater<>()));
-		for(auto&& person : people)
+		for(const auto& person : people)
 		{
 			std::cout << person.first_name << " " << person.last_name << ", " << person.age << "\n";
 		}
