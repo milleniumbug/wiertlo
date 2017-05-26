951310f4de8b608257ed9ab9d18ecd8a1f69f94e
milleniumbug <wojas1222@gmail.com>
2017-05-26 19:02:14 +0200

    lol this doesn't need to be a macro after all

--- examples/pretty_print_example.cpp
+++ examples/pretty_print_example.cpp
@@ -5,36 +5,31 @@
 #include <sstream>
 #include <iostream>
 
-void test_impl(const std::string& actual, const std::string& expected)
+template<typename T>
+void test(const T& value, const std::string& expected)
 {
+	auto actual = wiertlo::pretty::sprint<wiertlo::pretty::cpp_expression_format<>>(value); \
 	assert(actual == expected);
+	std::cout << actual << ";\n";
 }
-
-#define TEST(expr, expected) do \
-{\
-	auto actual = wiertlo::pretty::sprint<wiertlo::pretty::cpp_expression_format<>>(expr); \
-	test_impl(actual, expected); \
-	std::cout << actual << ";\n"; \
-}\
-while(false)
 
 int main()
 {
-	TEST(true, "true");
-	TEST(false, "false");
-	TEST(::std::string("\nhello world"), "::std::string(\"\\nhello world\")");
-	TEST(::std::string("\"hello\\world"), "::std::string(\"\\\"hello\\\\world\")");
-	TEST(::std::string("\"hello\\world"), "::std::string(\"\\\"hello\\\\world\")");
-	TEST('a', "\'a\'");
-	TEST('\n', "\'\\n\'");
-	TEST(42, "42");
-	TEST(42u, "42u");
-	TEST(42ul, "42ul");
-	TEST(42ull, "42ull");
-	TEST(static_cast<short>(42), "static_cast<short>(42)");
-	TEST(32768, "static_cast<int>(32768)");
-	TEST(-32767, "-32767");
-	TEST(-32768, "static_cast<int>(-32768)");
-	TEST(::std::make_unique<int>(42), "::std::make_unique<?!?!?!>(42)");
-	//TEST(::std::vector<int>({ 1, 2, 3, 4 }), "?!?!?!({1,2,3,4})");
+	test(true, "true");
+	test(false, "false");
+	test(::std::string("\nhello world"), "::std::string(\"\\nhello world\")");
+	test(::std::string("\"hello\\world"), "::std::string(\"\\\"hello\\\\world\")");
+	test(::std::string("\"hello\\world"), "::std::string(\"\\\"hello\\\\world\")");
+	test('a', "\'a\'");
+	test('\n', "\'\\n\'");
+	test(42, "42");
+	test(42u, "42u");
+	test(42ul, "42ul");
+	test(42ull, "42ull");
+	test(static_cast<short>(42), "static_cast<short>(42)");
+	test(32768, "static_cast<int>(32768)");
+	test(-32767, "-32767");
+	test(-32768, "static_cast<int>(-32768)");
+	test(::std::make_unique<int>(42), "::std::make_unique<?!?!?!>(42)");
+	//test(::std::vector<int>({ 1, 2, 3, 4 }), "?!?!?!({1,2,3,4})");
 }
