c2c47f6f00a26cb3c0fc9f59ba6854c2ba7d09c5
milleniumbug <wojas1222@gmail.com>
2017-05-26 18:43:15 +0200

    Simplify tests

--- examples/pretty_print_example.cpp
+++ examples/pretty_print_example.cpp
@@ -5,28 +5,36 @@
 #include <sstream>
 #include <iostream>
 
-void test(const std::string& actual, const std::string& expected)
+void test_impl(const std::string& actual, const std::string& expected)
 {
 	assert(actual == expected);
 }
 
+#define TEST(expr, expected) do \
+{\
+	auto actual = wiertlo::pretty::sprint<wiertlo::pretty::cpp_expression_format<>>(expr); \
+	test_impl(actual, expected); \
+	std::cout << actual << ";\n"; \
+}\
+while(false)
+
 int main()
 {
-	test(wiertlo::pretty::sprint<wiertlo::pretty::cpp_expression_format<>>(true), "true");
-	test(wiertlo::pretty::sprint<wiertlo::pretty::cpp_expression_format<>>(false), "false");
-	test(wiertlo::pretty::sprint<wiertlo::pretty::cpp_expression_format<>>(::std::string("\nhello world")), "::std::string(\"\\nhello world\")");
-	test(wiertlo::pretty::sprint<wiertlo::pretty::cpp_expression_format<>>(::std::string("\"hello\\world")), "::std::string(\"\\\"hello\\\\world\")");
-	test(wiertlo::pretty::sprint<wiertlo::pretty::cpp_expression_format<>>(::std::string("\"hello\\world")), "::std::string(\"\\\"hello\\\\world\")");
-	test(wiertlo::pretty::sprint<wiertlo::pretty::cpp_expression_format<>>('a'), "\'a\'");
-	test(wiertlo::pretty::sprint<wiertlo::pretty::cpp_expression_format<>>('\n'), "\'\\n\'");
-	test(wiertlo::pretty::sprint<wiertlo::pretty::cpp_expression_format<>>(42), "42");
-	test(wiertlo::pretty::sprint<wiertlo::pretty::cpp_expression_format<>>(42u), "42u");
-	test(wiertlo::pretty::sprint<wiertlo::pretty::cpp_expression_format<>>(42ul), "42ul");
-	test(wiertlo::pretty::sprint<wiertlo::pretty::cpp_expression_format<>>(42ull), "42ull");
-	test(wiertlo::pretty::sprint<wiertlo::pretty::cpp_expression_format<>>(static_cast<short>(42)), "static_cast<short>(42)");
-	test(wiertlo::pretty::sprint<wiertlo::pretty::cpp_expression_format<>>(32768), "static_cast<int>(32768)");
-	test(wiertlo::pretty::sprint<wiertlo::pretty::cpp_expression_format<>>(-32767), "-32767");
-	test(wiertlo::pretty::sprint<wiertlo::pretty::cpp_expression_format<>>(-32768), "static_cast<int>(-32768)");
-	test(wiertlo::pretty::sprint<wiertlo::pretty::cpp_expression_format<>>(::std::make_unique<int>(42)), "::std::make_unique<?!?!?!>(42)");
-	//test(wiertlo::pretty::sprint<wiertlo::pretty::cpp_expression_format<>>(::std::vector<int>({1,2,3,4})), "?!?!?!({1,2,3,4})");
+	TEST(true, "true");
+	TEST(false, "false");
+	TEST(::std::string("\nhello world"), "::std::string(\"\\nhello world\")");
+	TEST(::std::string("\"hello\\world"), "::std::string(\"\\\"hello\\\\world\")");
+	TEST(::std::string("\"hello\\world"), "::std::string(\"\\\"hello\\\\world\")");
+	TEST('a', "\'a\'");
+	TEST('\n', "\'\\n\'");
+	TEST(42, "42");
+	TEST(42u, "42u");
+	TEST(42ul, "42ul");
+	TEST(42ull, "42ull");
+	TEST(static_cast<short>(42), "static_cast<short>(42)");
+	TEST(32768, "static_cast<int>(32768)");
+	TEST(-32767, "-32767");
+	TEST(-32768, "static_cast<int>(-32768)");
+	TEST(::std::make_unique<int>(42), "::std::make_unique<?!?!?!>(42)");
+	//TEST(::std::vector<int>({ 1, 2, 3, 4 }), "?!?!?!({1,2,3,4})");
 }
